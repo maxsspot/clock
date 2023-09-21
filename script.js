37eed8f5f841c7066165fa1adc94faab022c9072
nope <55818423+maxsspot@users.noreply.github.com>
2023-09-21 16:31:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ document.getElementById("clock").style.backgroundColor = localStorage.getItem ("
 var loadedFont = localStorage.getItem ("loadFont");
 var loadedBackground = localStorage.getItem ("backgroundColor");
 
-            document.getElementById ("bgColor").value = loadedBackground;
-            document.body.style.backgroundColor = document.getElementById ("bgColor").value;
-
 document.addEventListener("DOMContentLoaded", function () {
     if (loadedFont == "Nunito Sans") {
         

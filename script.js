4c98753b29f9a614248f3191ec8a5485950020e8
nope <55818423+maxsspot@users.noreply.github.com>
2024-03-01 08:56:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -332,7 +332,7 @@ function custombg () {
     var custom = prompt('Please insert the URL of your image.');
 
     if (custom != null) {
-        document.body.style.background = "url(custom)";
+        document.body.style.background = "url('https://avatars.githubusercontent.com/u/55818423?v=4')";
         document.body.style.backgroundRepeat = "none";
         document.body.style.backgroundSize = "cover";
     }

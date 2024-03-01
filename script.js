f65b9c95a7b35891ba293db4c0e2c7687bc721d7
nope <55818423+maxsspot@users.noreply.github.com>
2024-03-01 15:03:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (loadedBgUrl) {
         document.body.style.background = "url(" + loadedBgUrl + ")";
-        document.body.style.backgroundSize = "cover";
+        document.body.style.backgroundSize = "100% 100%";
         document.body.style.backgroundRepeat = "no-repeat";
     }
 });
